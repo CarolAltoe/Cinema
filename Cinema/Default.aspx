bcb22fc3afef146c270a5125c2fc10560f7e9d22
Note191 <Note191@DESKTOP-DRJKS8S>
2023-11-13 18:10:40 -0300

    Adicionar arquivos de projeto.

---
+++ Cinema/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Cinema._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+  
+    <main>
+        <h1>Cine Girls</h1> <br />
+        <h2>Welcome!</h2>
+        heeyy jhennifeeerrrrrrrrr!!!!
+
+        <hr />
+
+        <div>
+            <asp:Label ID="Label2" runat="server" Text="Label">
+                Digite o nome do ator ou filme que deseja buscar:
+            </asp:Label> <br />
+            <input id="Text1" type="text" /> <br />
+            <asp:Button ID="bntBuscar" runat="server" Text="Buscar" /> 
+            <asp:Button ID="btnAddAtorFilme" runat="server" Text="Adicionar atuação (ator-filme)" /> <br />
+            
+        </div>
+
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label">Relação filmes e atores</asp:Label>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+       
+    </main>
+
+</asp:Content>
---
+++ Cinema/Default.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Cinema
+{
+
+
+    public partial class _Default
+    {
+
+        /// <summary>
+        /// Controle Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Controle bntBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button bntBuscar;
+
+        /// <summary>
+        /// Controle btnAddAtorFilme.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddAtorFilme;
+
+        /// <summary>
+        /// Controle Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Controle GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+    }
+}
